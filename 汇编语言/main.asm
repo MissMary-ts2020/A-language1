022478deaf341e835c1f0ff0fb540145e4a22386
可爱的魏氏大萌神 <hkgjwtl2017@aliyun.com>
2023-11-14 15:07:35 +0800

    15点07分2023年11月14日

--- 汇编语言/main.asm
+++ 汇编语言/main.asm
@@ -1,10 +1,21 @@
-.data
-sum DWORD 0 ;定义变量
-.code ;代码区
-main PROC
-    mov eax, 5
-    add eax, 6
-    mov sum, eax;eax读取到sum内部
 
-    INVOKE ExitProcess, 0;结束
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+
+.data
+sum DWORD 0 ;  hh定义变量
+;array DWORD 1111, 2222,5555,4444
+.code    ;代码区
+main PROC
+    mov ebx, 5
+    inc ebx
+    add ebx, 6
+    imul eax,ebx,3
+    mov sum, eax ;eax读取到sum内部
+    nop   ;这个完全没有用
+    INVOKE ExitProcess, 0  ;结束
 main ENDP
+end main
---
+++ 汇编语言/vb.vbs
@@ -0,0 +1,5 @@
+dim a,b,c as integer
+a=5
+b=6
+c=a+b
+msgbox(c)
